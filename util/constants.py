1e522b468e18252438494345ffbcb4c0120c3b5e
Aaron <aaron.valero@outlook.com>
2023-04-06 20:44:29 +0900

    [wip] changing constants

--- util/constants.py
+++ util/constants.py
@@ -3,16 +3,17 @@ from pathlib import Path
 # Grand Challenge folders were input files can be found
 
 # GC_CELL_FPATH = Path("/input/images/cell_patches/cell_patches.tif")
-GC_CELL_FPATH = Path("util/tiff_for_grand_challenge/cell_patches/cell_test.tif")
-# GC_CELL_FPATH = Path("test/cell_test.tif")
+# GC_CELL_FPATH = Path("util/tiff_for_grand_challenge/cell_patches/cell_test.tif")
+GC_CELL_FPATH = Path("test/input/images/cell_patches/000.tif")
 
 # GC_TISSUE_FPATH = Path("/input/images/tissue_patches/tissue_patches.tif")
-GC_TISSUE_FPATH = Path("util/tiff_for_grand_challenge/tissue_patches/tissue_test.tif")
-# GC_TISSUE_FPATH = Path("test/tissue_test.tif")
+# GC_TISSUE_FPATH = Path("util/tiff_for_grand_challenge/tissue_patches/tissue_test.tif")
+GC_TISSUE_FPATH = Path("test/input/images/tissue_patches/000.tif")
 
 # GC_METADATA_FPATH = Path("/input/metadata.json")
-GC_METADATA_FPATH = Path("util/ocelot2023_v1.0.0/metadata.json")
+# GC_METADATA_FPATH = Path("util/ocelot2023_v1.0.0/metadata.json")
+GC_METADATA_FPATH = Path("test/input/metadata.json")
 
 # Grand Challenge output file
 # GC_DETECTION_OUTPUT_PATH = Path("/output/cell_predictions.json")
-GC_DETECTION_OUTPUT_PATH = Path("cell_predictions.json")
+GC_DETECTION_OUTPUT_PATH = Path("test/output/cell_predictions.json")
