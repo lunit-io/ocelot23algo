d2a28bce634e022a1a81e2aed59ef2b13043d93a
Aaron Valero <aaron.valero@outlook.com>
2023-04-11 14:31:30 +0900

    remove unnecesary constants

--- util/constants.py
+++ util/constants.py
@@ -1,15 +1,11 @@
 from pathlib import Path
 
 # Grand Challenge folders were input files can be found
-# GC_CELL_FPATH = Path("/input/images/cell_patches/")
-GC_CELL_FPATH = Path("test/input/images/cell_patches/")
+GC_CELL_FPATH = Path("/input/images/cell_patches/")
 
-# GC_TISSUE_FPATH = Path("/input/images/tissue_patches/")
-GC_TISSUE_FPATH = Path("test/input/images/tissue_patches/")
+GC_TISSUE_FPATH = Path("/input/images/tissue_patches/")
 
-# GC_METADATA_FPATH = Path("/input/metadata.json")
-GC_METADATA_FPATH = Path("test/input/metadata.json")
+GC_METADATA_FPATH = Path("/input/metadata.json")
 
 # Grand Challenge output file
-# GC_DETECTION_OUTPUT_PATH = Path("/output/cell_predictions.json")
-GC_DETECTION_OUTPUT_PATH = Path("test/output/cell_predictions.json")
+GC_DETECTION_OUTPUT_PATH = Path("/output/cell_predictions.json")
